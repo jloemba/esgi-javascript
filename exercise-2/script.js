178bf8c9c7d7c2527c1cba89f279f0e6eb4f0515
Joris Loemba <jorisloemba@gmail.com>
2018-11-27 16:46:15 +0000

    TypeChech v1

---
+++ exercise-2/script.js
@@ -0,0 +1,17 @@
+
+
+
+function type_check_v1(value,expectedType){
+    console.log(typeof  value);
+    if(typeof value === expectedType) return true;
+    else return false;
+}
+console.log(type_check_v1(1,"number"));
+
+function type_check_v2(val,expectedType){
+
+
+
+}
+
+
